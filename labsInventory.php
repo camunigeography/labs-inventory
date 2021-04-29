ace56465fc96a9a2c350838cd225d327462c256a
mvl22 <martin@lucas-smith.co.uk>
2021-04-29 16:39:45 +0100

    Upgrade database joins to simpleJoin

--- labsInventory.php
+++ labsInventory.php
@@ -110,11 +110,11 @@ class labsInventory extends frontControllerApplication
 			  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Inventory no.',
 			  `name` varchar(255) NOT NULL COMMENT 'Name',
 			  `model` varchar(255) NOT NULL COMMENT 'Model',
-			  `type__JOIN__{$this->settings['database']}__equipmentType__reserved` int NOT NULL,
+			  `equipmentTypeId` int NOT NULL COMMENT 'Equipment type',
 			  `manufacturer` varchar(255) NOT NULL COMMENT 'Manufacturer',
 			  `quantity` int NOT NULL COMMENT 'Quantity held',
 			  `stockAvailable` int DEFAULT NULL COMMENT 'Stock available (0=no)',
-			  `location__JOIN__{$this->settings['database']}__locations__reserved` int DEFAULT NULL,
+			  `locationId` int DEFAULT NULL COMMENT 'Location',
 			  `serialNumber` text NOT NULL COMMENT 'Serial number',
 			  `notesPublic` text COMMENT 'Technical notes and specifications',
 			  `dateAcquired` date DEFAULT NULL COMMENT 'Date acquired',
@@ -145,7 +145,7 @@ class labsInventory extends frontControllerApplication
 			
 			CREATE TABLE `equipmentTypes` (
 			  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Unique key',
-			  `group__JOIN__{$this->settings['database']}__equipmentGroups__reserved` int NOT NULL COMMENT 'Group',
+			  `equipmentGroupId` int NOT NULL COMMENT 'Group',
 			  `type` varchar(255) NOT NULL COMMENT 'Type',
 			  `area` enum('','Field','Lab','Both') NOT NULL COMMENT 'Area',
 			  `notes` text NOT NULL COMMENT 'Notes',
@@ -498,8 +498,8 @@ class labsInventory extends frontControllerApplication
 			equipmentType.urlSlug,
 			locations.location
 		FROM equipment
-		LEFT JOIN equipmentType ON type__JOIN__{$this->settings['database']}__equipmentType__reserved = equipmentType.id
-		LEFT JOIN locations ON location__JOIN__{$this->settings['database']}__locations__reserved = locations.id
+		LEFT JOIN equipmentType ON equipmentTypeId = equipmentType.id
+		LEFT JOIN locations ON locationId = locations.id
 		WHERE
 			visibleOnline = 'Y'"
 			. ($equipmentTypeUrlSlug ? " AND urlSlug = " . $this->databaseConnection->quote ($equipmentTypeUrlSlug) : '')
@@ -510,8 +510,8 @@ class labsInventory extends frontControllerApplication
 		if (!$data = $this->databaseConnection->$function ($query, "{$this->settings['database']}.equipment")) {return false;}
 		
 		# Remove specific fields
-		unset ($data["type__JOIN__{$this->settings['database']}__equipmentType__reserved"]);
-		unset ($data["location__JOIN__{$this->settings['database']}__locations__reserved"]);
+		unset ($data['equipmentTypeId']);
+		unset ($data['locationId']);
 		
 		# Return the data
 		return $data;
@@ -527,8 +527,8 @@ class labsInventory extends frontControllerApplication
 			equipmentType.urlSlug,
 			COUNT(equipment.id) AS total
 		FROM equipment
-		LEFT JOIN equipmentType ON type__JOIN__{$this->settings['database']}__equipmentType__reserved = equipmentType.id
-		LEFT JOIN locations ON location__JOIN__{$this->settings['database']}__locations__reserved = locations.id
+		LEFT JOIN equipmentType ON equipmentTypeId = equipmentType.id
+		LEFT JOIN locations ON locationId = locations.id
 		WHERE
 			visibleOnline = 'Y'
 			AND equipmentType IS NOT NULL
@@ -1063,6 +1063,7 @@ Signature of staff ({$this->settings['labManagerNames']})
 	{
 		# Define sinenomine settings
 		$sinenomineExtraSettings = array (
+			'simpleJoin' => true,
 			'int1ToCheckbox' => true,
 			'datePicker' => true,
 			'richtextEditorToolbarSet' => 'BasicLonger',
